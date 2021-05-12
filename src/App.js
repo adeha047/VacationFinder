9d7467977643478a65763d0855ef3f9d1a78801c
adeha047 <dehaven7777@gmail.com>
2021-05-11 20:27:02 -0400

    added started code from instructor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Books from "./pages/Books";
+import Detail from "./pages/Detail";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
+import Login from "./pages/Login";
+import UserContext from "./utils/UserContext";
+import SignUp from "./pages/SignUp";
+import Logout from "./pages/Logout";
 
 function App() {
+  const [email, setEmail] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <UserContext.Provider value={{email, setEmail, loggedIn, setLoggedIn}}>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact path={["/", "/books"]}>
+              <Books />
+            </Route>
+            <Route exact path="/books/:bookid">
+              <Detail />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/signup">
+              <SignUp />
+            </Route>
+            <Route exact path="/logout">
+              <Logout />
+            </Route>
+            <Route>
+              <NoMatch />
+            </Route>
+          </Switch>
+        </div>
+      </UserContext.Provider>
+    </Router>
   );
 }
 
