f6d8eab9a36b2b0034d1b64cdfe52e4abc657da2
adeha047 <dehaven7777@gmail.com>
2021-05-13 21:50:55 -0400

    updated new API

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
 const bookRoutes = require("./books");
 const userRoutes = require("./user");
+const flightRoutes = require("./flights")
 
 // Book routes /api/books*
 router.use("/books", bookRoutes);
 // User routes /api/user*
 router.use("/user", userRoutes);
+
+router.use("/flights", flightRoutes);
 
 module.exports = router;
