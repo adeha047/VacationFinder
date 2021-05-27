9cd7d39199b8d345a0873012563fde40b426997d
Steven Woo <swoo9875@gmail.com>
2021-05-27 03:37:02 -0400

    added more style to the questionaire page

--- client/src/pages/Location.js
+++ client/src/pages/Location.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+// import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {FaPlane} from "react-icons/fa";
 
 // http://localhost:3001/api/interests
 function Location() {
@@ -54,17 +56,20 @@ function Location() {
         return (
             <>
                 <h2>Select Your City</h2>
-                <ul>
+                <div className = "Interest">
+                <ul className = "ask">
                     {locations.map((i) => (
                         <li key={i._id}>
-                            {i.location}
-                            <button type="button" className="btn btn-primary" style={{ marginRight: "25px" }} onClick={() => getFlight(i.location)}>Select Your Flight!</button>
+                            {i.location}  <button type="button" className="btn btn-primary" style={{ marginRight: "25px" }} onClick={() => getFlight(i.location)}><FaPlane/></button>    
                         </li>
                     ))}
                 </ul>
                 <div>
+                    <h2 className = "place">Selected Destination</h2>
                     <h3> {currentLocation} </h3>
+                    <h2 class = "airport">Airports</h2>
                     {flight.map((data) => (<p key={data.PlaceId}> {data.PlaceName}</p>))}
+                </div>
                 </div>
             </>
         )
