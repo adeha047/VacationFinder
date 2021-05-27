9e168fb6117a5c60dfb4bebd0b8378da32c8a9bc
Steven Woo <swoo9875@gmail.com>
2021-05-27 11:34:38 -0400

    add some fun styling to the typewriter

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import ReactTypingEffect from 'react-typing-effect';
+// import ReactTypingEffect from 'react-typing-effect';
+import Typewriter from "typewriter-effect";
 import bunny from "../images/bunny.gif";
 
 function Home() {
     return (
         <>
           <div class = "home">
-            <ReactTypingEffect
-                text="Hey Everyone!, Come Join Us On Our Adventure 
-                To Find The Perfect Vacation With Us" 
+            <Typewriter
+                onInit = {(typewriter) => {
+                    typewriter
+                        .typeString("Hey Everyone!")
+                        .pause(2000)
+                        .deleteAll()
+                        .typeString("Come Join Us On Our Adventure")
+                        .deleteAll()
+                        .typeString("To Find The Perfect Vacation With Us")
+                        .deleteAll()
+                        .start();
+                }}
             />
            </div> 
             <img src={bunny} alt="bunny" />
